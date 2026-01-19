ce1a80c0cd3de3446bc1c8c562e11185016250f1
hthinh24 <hungthinh240305@gmail.com>
2026-01-19 15:29:18 +0700

    add proofs

--- tuan02/bai01/src/App.jsx
+++ tuan02/bai01/src/App.jsx
@@ -3,17 +3,17 @@ import ProductCard from './components/ProductCard'
 import ipImg from './assets/ip16.png'
 const App = () => {
   return (
-    <ProductCard 
-      name = 'iPhone 17'
-      price = '999$'
-      src = {ipImg}
+    <ProductCard
+      name='iPhone 17'
+      price='999$'
+      src={ipImg}
 
-      productCard = 'product-card'
-      productImage = 'product-image'
-      productContent = 'product-content'
-      productTitle = 'product-title'
-      productPrice = 'product-price'
-      productButton = 'product-button'
+      productCard='product-card'
+      productImage='product-image'
+      productContent='product-content'
+      productTitle='product-title'
+      productPrice='product-price'
+      productButton='product-button'
     />
   )
 }
