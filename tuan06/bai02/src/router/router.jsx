468a61b9724e53c6c98e5119f37e25dc40bb4f55
H82M19\Student <Student>
2026-03-30 13:50:57 +0700

    bai02

---
+++ tuan06/bai02/src/router/router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../components/Home'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import App from '../App'
+import NotFoundPage from '../components/NotFoundPage'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App/>,
+        children:[
+            {
+                index: true,
+                element: <Home/>
+            },
+            {
+                path: '/about',
+                element: <About/>
+            },
+            {
+                path: '/contact',
+                element: <Contact/>
+            },
+        ]
+    },
+    {
+    path: '*',
+    element: <NotFoundPage />,
+  },
+])
